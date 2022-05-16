17f1ed656c43b4a8b6bc00da77b3641074a3ae11
ZeroIce <zeroicegc@163.com>
2022-05-16 23:16:08 +0800

    菜谱搭配-完

--- mini-merchant/miniprogram/pages/match/index.js
+++ mini-merchant/miniprogram/pages/match/index.js
@@ -1,20 +1,40 @@
 // pages/match/index.js
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    foods: []
   },
-  nav() {
+  nav(e) {
+    const { _id } = e.currentTarget.dataset;
     wx.navigateTo({
-      url: '../matchdetail/index',
+      url: `../matchdetail/index?_id=${_id}`,
     })
   },
   add() {
     wx.navigateTo({
       url: '../addmatch/index',
+    })
+  },
+  onShow() {
+    this.getData();
+  },
+  getData() {
+    let _this = this;
+    wx.showLoading({
+      title: '获取数据',
+      mask: true
+    })
+    db.collection('foods').get({
+      success: function (res) {
+        _this.setData({ foods: res.data })
+      },
+      complete() {
+        wx.hideLoading()
+      }
     })
   }
 })
--- mini-merchant/miniprogram/pages/matchdetail/index.js
+++ mini-merchant/miniprogram/pages/matchdetail/index.js
@@ -1,10 +1,34 @@
 // pages/matchdetail/index.js
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    foods: {}
+  },
+  onLoad(options) {
+    let { _id } = options;
+    this.getData(_id);
+  },
+  getData(_id) {
+    let _this = this;
+    wx.showLoading({
+      title: '获取数据',
+      mask: true
+    })
+    db.collection('foods').doc(_id).get({
+      success: function (res) {
+        _this.setData({
+          foods: res.data.foods, allHeat: res.data.foods.map(value=>value.heat).reduce((total, value) => {
+            return parseInt(total) + parseInt(value);
+          })
+        })
+      },
+      complete() {
+        wx.hideLoading()
+      }
+    })
   }
 })
