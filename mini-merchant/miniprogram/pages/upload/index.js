c6108ed3eb690ae0699b6b6875caaaa0843ea812
ZeroIce <zeroicegc@163.com>
2022-05-16 14:45:34 +0800

    上传商品-完

--- mini-merchant/miniprogram/pages/upload/index.js
+++ mini-merchant/miniprogram/pages/upload/index.js
@@ -1,15 +1,34 @@
 // pages/upload/index.js
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    foods: []
   },
-  add(){
+  add() {
     wx.navigateTo({
       url: '../addfood/index',
+    })
+  },
+  onShow() {
+    this.getData();
+  },
+  getData() {
+    let _this = this;
+    wx.showLoading({
+      title: '获取数据',
+      mask: true
+    })
+    db.collection('food').get({
+      success: function (res) {
+        _this.setData({ foods: res.data })
+      },
+      complete(){
+        wx.hideLoading()
+      }
     })
   }
 })
