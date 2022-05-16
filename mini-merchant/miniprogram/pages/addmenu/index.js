c132ce977e16f0ee94d418938d1f0cceb9c25388
ZeroIce <zeroicegc@163.com>
2022-05-17 00:28:42 +0800

    今日菜谱-完

--- mini-merchant/miniprogram/pages/addmenu/index.js
+++ mini-merchant/miniprogram/pages/addmenu/index.js
@@ -1,12 +1,19 @@
 // pages/record/index.js
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    active:-1,
+    active: -1,
     columns: ['早餐', '中餐', '晚餐', '其他餐'],
+    keyword: "",
+    searchs: [],
+    foods: {},
+    start: '',
+    end: '',
+    date: '选择日期',
   },
 
   /**
@@ -24,5 +31,66 @@ Page({
   onConfirm(event) {
     const { picker, value, index } = event.detail;
     this.setData({ active: index, show: false })
+  },
+  search() {
+    let _this = this;
+    const { keyword } = this.data;
+    // if (!keyword) {
+    //   wx.showToast({ title: '关键词不能为空', icon: "none" })
+    //   return;
+    // }
+    wx.showLoading({ title: '搜索中', mask: true })
+    db.collection('foods').where({
+      name: db.RegExp({
+        regexp: keyword,
+        option: 'i'
+      })
+    }).get({
+      success: function (res) {
+        _this.setData({ searchs: res.data })
+      },
+      complete() {
+        wx.hideLoading()
+      }
+    })
+  },
+  selected(e) {
+    const { item } = e.currentTarget.dataset;
+    this.setData({
+      foods: item
+    })
+  },
+  //选择日期
+  selectDate(e) {
+    let currentDate = e.detail.value;
+    this.setData({
+      date: currentDate
+    })
+  },
+  add() {
+    let _this = this;
+    const { foods, columns, active, date } = this.data;
+    if (!foods.price || active == -1 || date == "选择日期") {
+      wx.showToast({ title: '字段不能为空', icon: "none" })
+      return;
+    }
+    wx.showLoading({ title: '添加中', mask: true })
+    db.collection('menus').add({
+      // data 字段表示需新增的 JSON 数据
+      data: {
+        foods, date, type: columns[active]
+      },
+      success: function (res) {
+        _this.setData({
+          foods: {},
+          date: "选择日期",
+          active: -1,
+        })
+        wx.showToast({title: '添加成功',icon: 'none'})
+      },
+      complete() {
+        wx.hideLoading()
+      }
+    })
   }
 })
--- mini-merchant/miniprogram/pages/menu/index.js
+++ mini-merchant/miniprogram/pages/menu/index.js
@@ -1,4 +1,6 @@
 // pages/menu/index.js
+const db = wx.cloud.database()
+const { dateFormat } = require("../../utils/utils");
 Page({
 
   /**
@@ -8,7 +10,9 @@ Page({
     activeKey: 0,
     asides: ["早餐", "中餐", "晚餐", "其他餐"],
     height: 0,
-    isNotice: true
+    isNotice: true,
+    foods: [],
+    activeFoods: []
   },
 
   /**
@@ -22,23 +26,48 @@ Page({
       },
     })
   },
+  onShow() {
+    this.getData();
+  },
   closeNotice() {
     this.setData({
       isNotice: false
     })
   },
-  nav() {
-    let { activeKey } = this.data;
+  nav(e) {
+    let { _id } = e.currentTarget.dataset;
     wx.navigateTo({
-      url: `../matchdetail/index?active=${activeKey}`,
+      url: `../matchdetail/index?_id=${_id}`,
     })
   },
   onChange(event) {
     this.setData({ activeKey: event.detail })
   },
-  add(){
+  add() {
     wx.navigateTo({
       url: '../addmenu/index',
     })
+  },
+  getData() {
+    let _this = this;
+    let { activeKey, asides } = this.data;
+    wx.showLoading({ title: '获取数据', mask: true })
+    let nowDay = new Date();
+    db.collection('menus').where({
+      date: dateFormat("YYYY-mm-dd", nowDay)
+    }).get({
+      success: function (res) {
+        let activeFoods = res.data.filter(value => value.type == asides[activeKey])
+        _this.setData({ foods: res.data, activeFoods })
+      },
+      complete() {
+        wx.hideLoading()
+      }
+    })
+  },
+  onChange(event) {
+    let { foods, asides } = this.data;
+    let activeFoods = foods.filter(value => value.type == asides[event.detail])
+    this.setData({ activeFoods, activeKey: event.detail })
   }
 })
