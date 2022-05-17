4a11f0a1e7b1fbf72187f42baa2893bcab86fed5
ZeroIce <zeroicegc@163.com>
2022-05-17 16:25:08 +0800

    用户端-今日计划-完

--- mini/miniprogram/pages/today/index.js
+++ mini/miniprogram/pages/today/index.js
@@ -1,4 +1,6 @@
 // pages/today/index.js
+const db = wx.cloud.database()
+const { dateFormat } = require("../../utils/utils");
 Page({
 
   /**
@@ -6,7 +8,10 @@ Page({
    */
   data: {
     asides: ["早餐", "中餐", "晚餐", "其他餐"],
-    height: 0
+    active: 0,
+    height: 0,
+    activeFoods: {},
+    foods: []
   },
 
   /**
@@ -21,5 +26,28 @@ Page({
         })
       },
     })
+    this.getData();
+  },
+  getData() {
+    let _this = this;
+    let { asides, active } = this.data;
+    wx.showLoading({ title: '获取数据', mask: true })
+    let nowDay = new Date();
+    db.collection('plans').where({
+      date: dateFormat("YYYY-mm-dd", nowDay)
+    }).get({
+      success: function (res) {
+        let activeFoods = res.data.filter(value => value.type == asides[active])
+        _this.setData({ foods: res.data, activeFoods })
+      },
+      complete() {
+        wx.hideLoading()
+      }
+    })
+  },
+  onChange(event) {
+    let { foods, asides } = this.data;
+    let activeFoods = foods.filter(value => value.type == asides[event.detail.index])
+    this.setData({ activeFoods, active: event.detail.index })
   }
 })
